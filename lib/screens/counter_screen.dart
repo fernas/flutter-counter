3fcbdc8cae7e21c09fe79815eb4b10c049ae5a36
fernas <fernas@gmail.com>
2023-02-06 12:51:09 -0600

    Primer commit

---
+++ lib/screens/counter_screen.dart
@@ -0,0 +1,87 @@
+import 'package:flutter/material.dart';
+
+class CounterScreen extends StatefulWidget {
+  const CounterScreen({super.key});
+
+  @override
+  State<CounterScreen> createState() => _CounterScreenState();
+}
+
+class _CounterScreenState extends State<CounterScreen> {
+  int counter = 10;
+
+  void increase() {
+    counter++;
+    setState(() {});
+  }
+
+  void decrease() {
+    counter--;
+    setState(() {});
+  }
+
+  void reset() {
+    counter = 0;
+    setState(() {});
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    const fontSize30 = TextStyle(fontSize: 30);
+
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Counter Screen'),
+        elevation: 0,
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            const Text('Número de clicks', style: fontSize30),
+            Text('$counter', style: fontSize30)
+          ],
+        ),
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
+      floatingActionButton: CustomFloatingActions(
+        increaseFn: increase,
+        decreaseFn: decrease,
+        resetFn: reset,
+      ),
+    );
+  }
+}
+
+class CustomFloatingActions extends StatelessWidget {
+  final Function increaseFn;
+  final Function decreaseFn;
+  final Function resetFn;
+
+  const CustomFloatingActions(
+      {super.key,
+      required this.increaseFn,
+      required this.decreaseFn,
+      required this.resetFn});
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+      children: [
+        FloatingActionButton(
+          child: const Icon(Icons.exposure_plus_1_outlined),
+          onPressed: () => increaseFn(),
+        ),
+        FloatingActionButton(
+          child: const Icon(Icons.car_crash),
+          onPressed: () => resetFn(),
+        ),
+        FloatingActionButton(
+          child: const Icon(Icons.exposure_minus_1_outlined),
+          onPressed: () => decreaseFn(),
+        ),
+      ],
+    );
+  }
+}
